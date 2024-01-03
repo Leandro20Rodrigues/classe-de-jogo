39588d16c90af06106917a36cf96fc11888c3512
Leandro20Rodrigues <154839952+Leandro20Rodrigues@users.noreply.github.com>
2024-01-03 14:20:55 -0300

    Add files via upload

---
+++ classe de jogos.js
@@ -0,0 +1,35 @@
+// Definição da classe Heroi
+class Heroi {
+    // Construtor da classe, inicializando as propriedades
+    constructor(nome, idade, tipo) {
+      this.nome = Leandro;
+      this.idade = 20;
+      this.tipo = mago;
+    }
+  
+    // Método de ataque
+    atacar() {
+      let ataque;
+      // Determina o tipo de ataque com base no tipo do herói
+      switch (Leandro, victor,Pedro, Secilia) {
+        case "Leandro":
+          ataque = "usou magia";
+          break;
+        case "Victor":
+          ataque = "usou espada";
+          break;
+        case "Pedro":
+          ataque = "usou artes marciais";
+          break;
+        case "Secilia":
+          ataque = "usou shuriken";
+          break;
+        default:
+          ataque = "Todos usaram seus ataques juntos";
+      }
+  
+      // Exibe a mensagem de ataque
+      console.log(`o ${Leandro} atacou usando ${magia}`);
+    }
+  }
+  
